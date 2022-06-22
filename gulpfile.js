f54a57f44a79be3ae3dfdfef00d79d44df479312
Fabrizio <fabrinucci99@gmail.com>
2022-06-22 18:46:08 -0300

    festival-musica creado

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+const { series, src, dest, watch, parallel } = require('gulp');
+const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const webp = require('gulp-webp');
+const concat = require('gulp-concat');
+
+// Utilidades Css
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+
+//Utilidades Js
+const terser = require('gulp-terser-js');
+const rename = require('gulp-rename');
+
+//funcion que compila sass
+
+const paths = {
+    imagenes: 'src/img/**/*',
+    scss: 'src/scss/**/*.scss',
+    js: 'src/js/**/*.js'
+}
+
+function css() {
+    return src(paths.scss)
+    .pipe( sourcemaps.init() )
+    .pipe( sass() )
+    .pipe( postcss([ autoprefixer(), cssnano() ]))
+    .pipe( sourcemaps.write('.') )
+    .pipe( dest('./build/css') )
+}
+
+function minificarcss() {
+    return src(paths.scss)
+    .pipe( sass({
+        outputStyle: 'compressed'
+    }) )
+    .pipe( dest('./build/css') )
+}
+
+function javascript() {
+    return src(paths.js)
+        .pipe( sourcemaps.init() )
+        .pipe( concat('bundle.js') )
+        .pipe( terser() )
+        .pipe( rename({ suffix: '.min'}))
+        .pipe( sourcemaps.write('.'))
+        .pipe( dest('./build/js') )
+}
+
+function imagenes() {
+    return src(paths.imagenes)
+        .pipe( imagemin() )
+        .pipe( dest( './build/img' ))
+        .pipe( notify({ message: 'Imagen Minificada' }) );
+}
+
+function versionWebp() {
+    return src(paths.imagenes)
+        .pipe( webp() )
+        .pipe( dest('./build/img'))
+        .pipe( notify({message: 'Version webP'}))
+}
+
+function watchArchivos() {
+    watch(paths.scss, css); // * = La carpeta actual - ** = Todos los archivos con esa extension
+    watch(paths.js, javascript);
+}
+
+exports.css = css;
+exports.minificarcss = minificarcss;
+exports.imagenes= imagenes;
+exports.watchArchivos = watchArchivos;
+
+exports.default = series( css, javascript, imagenes, versionWebp, watchArchivos);
